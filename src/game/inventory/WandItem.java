a0f8cf3885854cc04a9946c045a7d7a467ee0f47
Jake <Jake@LAPTOP-O13VNB60>
2018-08-31 19:32:26 -0700

    Added new spell effect: WindGust. Weaker than FireIgnite, more knockback. Spell switching done by physically manipulating code. Wand currently set to WindGust by default

--- src/game/inventory/WandItem.java
+++ src/game/inventory/WandItem.java
@@ -5,7 +5,7 @@ import game.spells.SpellInfo;
 import game.spells.SpellInfo.SpellTarget;
 import game.spells.SpellPart.SpellShapeInitial;
 import static game.spells.TypeDefinitions.constructSpell;
-import game.spells.effects.FireIgnite;
+import game.spells.effects.*;
 import game.spells.shapes.S_Burst;
 import game.spells.shapes.S_Projectile;
 import game.spells.shapes.SpellShapeMissile;
@@ -17,7 +17,7 @@ public class WandItem extends UsableItem {
     public void useItemPress(Player player, boolean isMainHand) {
         SpellShapeMissile missile = new S_Projectile();
         missile.isMultishot = true;
-        SpellShapeInitial shape = constructSpell(missile, new S_Burst(), new FireIgnite());
+        SpellShapeInitial shape = constructSpell(missile, new S_Burst(), new WindGust());
         SpellInfo info = new SpellInfo(new SpellTarget(Camera.camera3d.position.add(MathUtils.randomInSphere().mul(.1))), Camera.camera3d.facing(), 1, player.physics.world);
         shape.cast(info);
     }

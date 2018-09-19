8d2156e45cd347ae2948aceb04d78f0414434b4c
Weirdbob95 <rory.soiffer@comcast.net>
2018-09-18 17:40:48 -0700

    Documented Status.java

--- src/game/combat/Status.java
+++ src/game/combat/Status.java
@@ -8,29 +8,68 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * The Status class represents any status effect on a creature with a duration
+ * in seconds.
+ *
+ * @author Rory
+ */
 public abstract class Status {
 
+    /**
+     * The creature that this status affects.
+     */
     public final CreatureBehavior creature;
 
     private final List<Modifier> modifiers = new LinkedList();
     private final double maxTimer;
     private double timer;
 
+    /**
+     * Constructs a new Status that targets the given creature and lasts the
+     * given amount of time.
+     *
+     * @param creature The creature the status should affect
+     * @param maxTimer The duration in seconds of the status
+     */
     public Status(CreatureBehavior creature, double maxTimer) {
         this.creature = creature;
         this.maxTimer = maxTimer;
     }
 
+    /**
+     * Attaches the given modifiers to this status, so that the modifiers will
+     * be removed when this status ends.
+     *
+     * @param m The modifiers to add
+     */
     protected void addModifiers(Modifier... m) {
         modifiers.addAll(Arrays.asList(m));
     }
 
+    /**
+     * This function is called when the status takes effect.
+     */
     protected abstract void onStart();
 
+    /**
+     * This function is called every frame while the status is active.
+     *
+     * @param dt The amount of time in seconds since the last frame
+     */
     protected abstract void onUpdate(double dt);
 
+    /**
+     * This function specifies how this status interacts with existing statuses
+     * of the same type that target the same creature.
+     *
+     * @return The StackMode that specifies this status's behavior
+     */
     protected abstract StackMode stackMode();
 
+    /**
+     * This function makes the status start affecting its target.
+     */
     public void start() {
         if (stackMode() != SEPARATE) {
             for (Status s : creature.statuses) {
@@ -49,6 +88,12 @@ public abstract class Status {
         onStart();
     }
 
+    /**
+     * This function updates the status, and removes it once its duration is
+     * finished.
+     *
+     * @param dt The amount of time in seconds since the last frame
+     */
     public void update(double dt) {
         timer -= dt;
         if (timer <= 0) {
@@ -59,9 +104,27 @@ public abstract class Status {
         }
     }
 
+    /**
+     * This enum defines how a status should interact with existing status of
+     * the same type affecting the same creature.
+     */
     public static enum StackMode {
+
+        /**
+         * When there are two statuses of the same type affecting the same
+         * creature, combine them into a single status with a duration of both
+         * durations combined.
+         */
         ADD_DURATION,
+        /**
+         * When there are two statuses of the same type affecting the same
+         * creature, keep the one with the longer duration and remove the other.
+         */
         MAX_DURATION,
+        /**
+         * When there are two statuses of the same type affecting the same
+         * creature, keep both, just as if they were different types.
+         */
         SEPARATE
     }
 }
